c81da9b3a68d636357ae4a1fdf9b19aed1ee0bb6
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-26 06:45:38 -0300

    projeto inacabado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,21 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { Home } from '../src/app/index.tsx'
+import { Home } from "../../src/app/(tabs)/index";
+import { Search } from "../../src/app/(tabs)/search";
+import { Inbox } from "../../src/app/(tabs)/inbox";
+import { New } from "../../src/app/(tabs)/new";
+import { Profile } from "../../src/app/(tabs)/profile";
+
+const Tab = createBottomTabNavigator();
+
+export function Routes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="New" component={New} />
+      <Tab.Screen name="Inbox" component={Inbox} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
